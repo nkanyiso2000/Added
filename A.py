fbea18f4f95f2ad1036237f15c55b7e1ac65299b
= <=>
2024-09-26 16:22:29 +0200

    LAMULA

---
+++ A.py
@@ -0,0 +1,8 @@
+Average_score = int(input("Please enter your Score: "))
+
+if Average_score > 80:
+    print("Congratulations!! You've passed your Grades with an A.")
+elif Average_score >= 50:
+    print("You've passed your Grades.")
+else:
+    print("You failed your Grades.")
